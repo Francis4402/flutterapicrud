cbb01f1821dd347d26caad018fabeb3c231ceb15
Francis4402 <francism4402@gmail.com>
2025-06-05 19:10:27 +0600

    post routes updated

--- src/server.ts
+++ src/server.ts
@@ -58,6 +58,7 @@ async function main() {
                 
                 
                 await newMessage.save();
+                
 
                 io.to(roomId).emit('newMessage', {
                     _id: newMessage._id,
