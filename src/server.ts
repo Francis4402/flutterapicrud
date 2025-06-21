c82a458088838c3208e1d854851fb122cb2f93ea
Francis4402 <francism4402@gmail.com>
2025-06-21 19:03:54 +0600

    server codes update

--- src/server.ts
+++ src/server.ts
@@ -20,154 +20,154 @@ async function main() {
             console.log(`Server is running on port ${config.port}`);
         })
 
-        const io = new SocketIOServer(server, {
-            cors: {
-                origin: "*",
-                methods: ["GET", "POST"]
-            }
-        });
+        // const io = new SocketIOServer(server, {
+        //     cors: {
+        //         origin: "*",
+        //         methods: ["GET", "POST"]
+        //     }
+        // });
 
-        io.on("connection", (socket) => {
-            console.log("✅ Client connected:", socket.id);
+        // io.on("connection", (socket) => {
+        //     console.log("✅ Client connected:", socket.id);
 
-            socket.on('joinRoom', (roomId) => {
-                socket.join(roomId);
-                console.log(`User ${socket.id} joined room ${roomId}`);
-            });
+        //     socket.on('joinRoom', (roomId) => {
+        //         socket.join(roomId);
+        //         console.log(`User ${socket.id} joined room ${roomId}`);
+        //     });
 
-            socket.on('sendMessage', async (data) => {
+        //     socket.on('sendMessage', async (data) => {
    
-                const { roomId, senderId, receiverId, message, image, file, fileName } = data;
+        //         const { roomId, senderId, receiverId, message, image, file, fileName } = data;
 
-                let imagePath: string | undefined;
-                let filePath: string | undefined;
+        //         let imagePath: string | undefined;
+        //         let filePath: string | undefined;
 
-                if(image) {
-                    const extension = image.startsWith('data:image/')
-                    ? image.split(';')[0].split('/')[1] : '*';
-                    imagePath = saveFile(image, extension);
-                }
+        //         if(image) {
+        //             const extension = image.startsWith('data:image/')
+        //             ? image.split(';')[0].split('/')[1] : '*';
+        //             imagePath = saveFile(image, extension);
+        //         }
 
-                if (file) {
-                    const extension = fileName?.split('.').pop() || 'bin';
-                    filePath = saveFile(file, extension);
-                }
+        //         if (file) {
+        //             const extension = fileName?.split('.').pop() || 'bin';
+        //             filePath = saveFile(file, extension);
+        //         }
 
-                const newMessage = new MessageModel({
-                    roomId,
-                    senderId,
-                    receiverId,
-                    message,
-                    image,
-                    file: filePath,
-                    fileName,
-                    timestamp: new Date(),
-                });
+        //         const newMessage = new MessageModel({
+        //             roomId,
+        //             senderId,
+        //             receiverId,
+        //             message,
+        //             image,
+        //             file: filePath,
+        //             fileName,
+        //             timestamp: new Date(),
+        //         });
                 
                 
-                await newMessage.save();
+        //         await newMessage.save();
                 
 
-                io.to(roomId).emit('newMessage', {
-                    _id: newMessage._id,
-                    roomId,
-                    senderId,
-                    receiverId,
-                    message,
-                    image,
-                    file: filePath,
-                    fileName,
-                    timestamp: newMessage.timestamp,
-                });
+        //         io.to(roomId).emit('newMessage', {
+        //             _id: newMessage._id,
+        //             roomId,
+        //             senderId,
+        //             receiverId,
+        //             message,
+        //             image,
+        //             file: filePath,
+        //             fileName,
+        //             timestamp: newMessage.timestamp,
+        //         });
                   
-            });
+        //     });
 
-            socket.on('markAsRead', async ({ roomId, senderId, receiverId }) => {
-                try {
-                  await MessageModel.updateMany(
-                    {
-                      roomId,
-                      senderId,
-                      receiverId,
-                      isRead: false,
-                    },
-                    { $set: { isRead: true } }
-                  );
+        //     socket.on('markAsRead', async ({ roomId, senderId, receiverId }) => {
+        //         try {
+        //           await MessageModel.updateMany(
+        //             {
+        //               roomId,
+        //               senderId,
+        //               receiverId,
+        //               isRead: false,
+        //             },
+        //             { $set: { isRead: true } }
+        //           );
               
-                  io.to(roomId).emit('messagesRead', {
-                    senderId,
-                    receiverId,
-                  });
+        //           io.to(roomId).emit('messagesRead', {
+        //             senderId,
+        //             receiverId,
+        //           });
               
-                  const unreadCount = await MessageModel.countDocuments({
-                    receiverId,
-                    isRead: false,
-                  });
-                  io.to(receiverId).emit('unreadCountUpdate', { unreadCount });
-                } catch (err) {
-                  console.error("Error marking messages as read:", err);
-                }
-            });
+        //           const unreadCount = await MessageModel.countDocuments({
+        //             receiverId,
+        //             isRead: false,
+        //           });
+        //           io.to(receiverId).emit('unreadCountUpdate', { unreadCount });
+        //         } catch (err) {
+        //           console.error("Error marking messages as read:", err);
+        //         }
+        //     });
               
               
 
-            socket.on('deleteMessage', async (data) => {
-                try {
-                    const { id, roomId, senderId } = data;
+        //     socket.on('deleteMessage', async (data) => {
+        //         try {
+        //             const { id, roomId, senderId } = data;
                     
-                    console.log(data);
-                    const message = await MessageModel.findById(id);
+        //             console.log(data);
+        //             const message = await MessageModel.findById(id);
 
-                    if (!message) {
-                        console.log('[WARNING] Message not found');
-                        return;
-                    }
+        //             if (!message) {
+        //                 console.log('[WARNING] Message not found');
+        //                 return;
+        //             }
 
-                    if (message.senderId.toString() !== senderId) {
-                        console.log('[ERROR] Unauthorized deletion attempt');
-                        return;
-                    }
+        //             if (message.senderId.toString() !== senderId) {
+        //                 console.log('[ERROR] Unauthorized deletion attempt');
+        //                 return;
+        //             }
 
-                    if (message.image) {
-                        console.log(`[INFO] Attempting to delete image: ${message.image}`);
-                        const imageDeleted = deleteFile(message.image);
-                        if (!imageDeleted) {
-                            console.log('[WARNING] Image deletion failed');
-                        }
-                    }
+        //             if (message.image) {
+        //                 console.log(`[INFO] Attempting to delete image: ${message.image}`);
+        //                 const imageDeleted = deleteFile(message.image);
+        //                 if (!imageDeleted) {
+        //                     console.log('[WARNING] Image deletion failed');
+        //                 }
+        //             }
 
-                    if (message.file) {
-                        console.log(`[INFO] Attempting to delete file: ${message.file}`);
-                        const fileDeleted = deleteFile(message.file);
-                        if (!fileDeleted) {
-                            console.log('[WARNING] File deletion failed');
-                        }
-                    }
+        //             if (message.file) {
+        //                 console.log(`[INFO] Attempting to delete file: ${message.file}`);
+        //                 const fileDeleted = deleteFile(message.file);
+        //                 if (!fileDeleted) {
+        //                     console.log('[WARNING] File deletion failed');
+        //                 }
+        //             }
 
-                    const deletedMessage = await MessageModel.findOneAndDelete({
-                        _id: id,
-                        senderId: senderId
-                    });
+        //             const deletedMessage = await MessageModel.findOneAndDelete({
+        //                 _id: id,
+        //                 senderId: senderId
+        //             });
                     
-                    if (deletedMessage) {
-                        io.to(roomId).emit('messageDeleted', { 
-                            id: deletedMessage._id.toString()
-                        });
-                    }
+        //             if (deletedMessage) {
+        //                 io.to(roomId).emit('messageDeleted', { 
+        //                     id: deletedMessage._id.toString()
+        //                 });
+        //             }
 
                     
-                } catch (error) {
-                    console.log(error);
-                }
-            });
+        //         } catch (error) {
+        //             console.log(error);
+        //         }
+        //     });
 
             
 
-            socket.on("disconnect", () => {
-                console.log("❌ Client disconnected:", socket.id);
-            });
+        //     socket.on("disconnect", () => {
+        //         console.log("❌ Client disconnected:", socket.id);
+        //     });
 
-        });
+        // });
         
     } catch (error) {
         console.log(error);
